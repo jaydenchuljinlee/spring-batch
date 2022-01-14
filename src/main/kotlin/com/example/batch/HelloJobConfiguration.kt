42885aa954781c6c68810b86d58715da8a082a12
cheojlin <asddqe111@bbrick.com>
2022-01-15 00:16:11 +0900

    스프링 배치 실행 - Job

--- src/main/kotlin/com/example/batch/HelloJobConfiguration.kt
+++ src/main/kotlin/com/example/batch/HelloJobConfiguration.kt
@@ -5,8 +5,8 @@ import org.springframework.batch.core.Step
 import org.springframework.batch.core.StepContribution
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.launch.support.RunIdIncrementer
 import org.springframework.batch.core.scope.context.ChunkContext
-import org.springframework.batch.core.step.tasklet.Tasklet
 import org.springframework.batch.repeat.RepeatStatus
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -16,4 +16,33 @@ data class HelloJobConfiguration(
     val jobBuilderFactory: JobBuilderFactory,
     val stepBuilderFactory: StepBuilderFactory
 ) {
+    @Bean
+    fun batchJob(): Job? {
+        return jobBuilderFactory.get("batchJob")
+            .incrementer(RunIdIncrementer())
+            .start(step1())
+            .next(step2())
+            .build()
+    }
+
+    @Bean
+    fun step1(): Step {
+        return stepBuilderFactory.get("step1")
+            .tasklet{ stepContribution: StepContribution, chunkContext: ChunkContext ->
+                println("step1 has executed")
+                return@tasklet RepeatStatus.FINISHED
+            }.build()
+    }
+
+    @Bean
+    fun step2(): Step {
+        return stepBuilderFactory.get("step1")
+            .tasklet{ stepContribution: StepContribution, chunkContext: ChunkContext ->
+                println("step2 has executed")
+                return@tasklet RepeatStatus.FINISHED
+            }.build()
+    }
+
 }
+
+
